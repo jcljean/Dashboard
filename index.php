4cd469df79244c037ce976861f8aacdd8357ceef
Jean Livero <jlivero@icloud.com>
2026-03-09 22:16:43 -0300

    feat: implementada tabela de gestão de usuários

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboards</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
 <body>
     <div class="dashboard-container">
@@ -26,7 +27,7 @@
                 <article>
                     <h2>Módulo de usuários</h2>
                     <p>Gerencie os acessos e permissões do sistema nesta área</p>
-                    <a href="#" class ="btn">Acessar</a>
+                    <a href="usuario.php" class ="btn">Acessar</a>
                 </article>
                 <article>
                     <h2>Relatórios Gerenciais</h2>
